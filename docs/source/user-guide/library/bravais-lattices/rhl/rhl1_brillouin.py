a9a0506e1d64d660fdfe3c72a541afd658988cb1
Andrey Rybakov <rybakov.ad@icloud.com>
2023-12-10 17:30:02 +0100

    Remove license header from the bravais lattice examples

--- docs/source/user-guide/library/bravais-lattices/rhl/rhl1_brillouin.py
+++ docs/source/user-guide/library/bravais-lattices/rhl/rhl1_brillouin.py
@@ -1,25 +1,7 @@
-# WULFRIC - Crystal, Lattice, Atoms, K-path.
-# Copyright (C) 2023 Andrey Rybakov
-#
-# e-mail: anry@uv.es, web: adrybakov.com
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import wulfricshorttools as wulfricshort
 
-import wulfric as rad
-
-l = rad.lattice_example("RHL1")
-backend = rad.PlotlyBackend()
+l = wulfricshort.lattice_example("RHL1")
+backend = wulfricshort.PlotlyBackend()
 backend.plot(l, kind="brillouin-kpath")
 # Save an image:
 backend.save("rhl1_brillouin.png")
