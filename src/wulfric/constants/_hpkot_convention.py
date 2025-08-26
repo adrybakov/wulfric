117647d3346e6ec694224829e17d095963f95900
Andrey Rybakov <rybakov.ad@icloud.com>
2025-08-26 14:29:27 +0200

    Write down HPKOT paths

--- src/wulfric/constants/_hpkot_convention.py
+++ src/wulfric/constants/_hpkot_convention.py
@@ -137,6 +137,37 @@ HPKOT_CONVENTIONAL_TO_PRIMITIVE = {
     ),
 }
 
+HPKOT_DEFAULT_K_PATHS = {
+    "cP1": "G-X-M-G-R-X|R-M-X1",
+    "cP2": "G-X-M-G-R-X|R-M",
+    "cF1": "G-X-U|K-G-L-W-X-W2",
+    "cF2": "G-X-U|K-G-L-W-X",
+    "cI1": "G-H-N-G-P-H|P-N",
+    "tP1": "G-X-M-G-Z-R-A-Z|X-R|M-A",
+    "tI1": "G-X-M-G-Z|Z0-M|X-P-N-G",
+    "tI2": "G-X-P-N-G-M-S|S0-G|X-R|G-M",
+    "oP1": "G-X-S-Y-G-Z-U-R-T-Z|X-U|Y-T|S-R",
+    "oF1": "G-Y-T-Z-G-SIGMA0|U0-T|Y-C0|A0-Z|G-L",
+    "oF2": "G-T-Z-Y-G-LAMBDA0|Q0-Z|T-G0|H0-Y|G-L",
+    "oF3": "G-Y-C0|A0-Z-B0|D0-T-G0|H0-Y|T-G-Z|G-L",
+    "oI1": "G-X-F2|SIGMA0-G-Y0|U0-X|G-R-W-S-G-T-W",
+    "oI2": "G-X-U2|Y0-G-LAMBDA0|G2-X|G-R-W-S-G-T-W",
+    "oI3": "G-X-F0|SIGMA0-G-LAMBDA0|G0-X|G-R-W-S-G-T-W",
+    "oC1": "G-Y-C0|SIGMA0-G-Z-A0|E0-T-Y|G-S-R-Z-T",
+    "oC2": "G-Y-F0|DELTA0-G-Z-B0|G0-T-Y|G-S-R-Z-T",
+    "oA1": "G-Y-C0|SIGMA0-G-Z-A0|E0-T-Y|G-S-R-Z-T",
+    "oA2": "G-Y-F0|DELTA0-G-Z-B0|G0-T-Y|G-S-R-Z-T",
+    "hP1": "G-M-K-G-A-L-H-A|L-M|H-K-H2",
+    "hP2": "G-M-K-G-A-L-H-A|L-M|H-K",
+    "hR1": "G-T-H2|H0-L-G-S0|S2-F-G",
+    "hR2": "G-L-T-P0|P2-G-F",
+    "mP1": "G-Z-D-B-G-A-E-Z-C2-Y2-G",
+    "mC1": "G-C|C2-Y2-G-M2-D|D2-A-G|L2-G-V2",
+    "mC2": "G-Y-M-A-G|L2-G-V2",
+    "mC3": "G-A-I2|I-M2-G-Y|L2-G-V2",
+    "aP2": "G-X|Y-G-Z|R-G-T|U-G-V",
+    "aP3": "G-X|Y-G-Z|R2-G-T2|U2-G-V2",
+}
 
 # Populate __all__ with objects defined in this file
 __all__ = list(set(dir()) - old_dir)
