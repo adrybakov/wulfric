e8954c5450f0123ad9209ca3e318dfa2de101bb4
Andrey Rybakov <rybakov.ad@icloud.com>
2025-09-05 23:46:52 +0200

    Update release metadata

--- src/wulfric/__init__.py
+++ src/wulfric/__init__.py
@@ -18,9 +18,9 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 # ================================ END LICENSE =================================
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 __doclink__ = "wulfric.org"
-__release_date__ = "29 August 2025"
+__release_date__ = "5 September 2025"
 
 
 from . import cell, constants, crystal, geometry, io, kpoints
